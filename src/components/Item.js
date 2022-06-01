6f1a1228c4fc2e062a1ffddececb83badfa8719c
martin <acepecs@gmail.com>
2022-06-01 18:55:44 -0300

    Item

---
+++ src/components/Item.js
@@ -0,0 +1,22 @@
+
+import { Link } from 'react-router-dom';
+
+
+const Item = ({ id, name, stock, cost, pictureUrl }) => {
+    return (
+        <div>
+            <div />
+            <div src={pictureUrl} />
+            <div>
+                <div>
+                    <div /><strong>$ {cost}</strong>
+                </div>
+                <div>
+                    <div/>{stock} unid.
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Item;
