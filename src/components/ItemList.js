e009d4d35f4b642bffb74a2ae044685d87176dfb
martin <acepecs@gmail.com>
2022-06-06 18:35:48 -0300

    ItemList

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -4,7 +4,7 @@ const ItemList = ({ items }) => {
         <div>
         {
             items.length > 0
-            ? items.map(item => <Item key={item.id} id={item.id} title={item.name} price={item.cost} pictureUrl={item.img[0]} stock={item.stock} />)
+            ? items.map(item => <Item key={item.id} id={item.id} title={item.name} price={item.cost} pictureUrl={item.pictureUrl} stock={item.stock} />)
             : <p>Cargando...</p>
         }
         </div>
